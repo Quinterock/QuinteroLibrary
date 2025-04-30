5c663a907a320398f8aae348699fb72b790c4ed2
Quinterock <luisgqr1975@gmail.com>
2025-04-30 17:06:46 -0600

    Implemented KeyChain

---
+++ Sources/QuinteroLibrary/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  QuinteroLibrary
+//
+//  Created by Luis Quintero on 29/04/25.
+//
+
+import Foundation
+
+enum Constants {
+    static let myKeychain = "LOGIN_SERVICE_KEY"
+    
+}
