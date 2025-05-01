d089723ddf3d5fb37559451681fc65581a2764fb
Quinterock <luisgqr1975@gmail.com>
2025-05-01 16:36:53 -0600

    Added keychainmanager

--- Sources/QuinteroLibrary/KeyChainKC.swift
+++ Sources/QuinteroLibrary/KeyChainKC.swift
@@ -3,22 +3,47 @@
 
 import KeychainAccess
 
-protocol KeyChainKC {
+public protocol KeyChainKC {
     func setKey(key: String, value: String) -> Bool
     func getKC(key: String) -> String?
     func removeKC(key: String) -> Bool
 }
 
-struct KeyChainLibrary: KeyChainKC {
-    func setKey(key: String, value: String) -> Bool {
+public class KeyChainManager: KeyChainKC {
+    static var shared: KeyChainKC {
+        return KeyChainManager()
+    }
+    
+    public init() {}
+    
+    public func setKey(key: String, value: String) -> Bool {
         saveKC(key: key, value: value)
     }
     
-    func getKC(key: String) -> String? {
+    public func getKC(key: String) -> String? {
         loadKC(key: key)
     }
     
-    func removeKC(key: String) -> Bool {
+    public func removeKC(key: String) -> Bool {
+        deleteKC(key: key)
+    }
+    
+}
+
+// Alternativa sin manager
+public struct KeyChain: KeyChainKC {
+    
+    public init() {}
+    
+    public func setKey(key: String, value: String) -> Bool {
+        saveKC(key: key, value: value)
+    }
+    
+    public func getKC(key: String) -> String? {
+        loadKC(key: key)
+    }
+    
+    public func removeKC(key: String) -> Bool {
         deleteKC(key: key)
     }
 }
